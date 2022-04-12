18a65784dc26a9fc3425e43b2a0cb64c611ddb99
9177siva <sivannarayana49@gmail.com>
2022-04-12 18:39:46 +0530

    changed num1 & num2 values in siva branch

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void main(string args[])
 {
 	int num1,num2,res;
-	num1=20;
-	num2=45;
+	num1=2005;
+	num2=4508;
 	res=num1+num2;
 	system.out.println("The sum is "+res);
 	//this is comment
