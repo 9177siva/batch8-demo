ef57dc19800647eb4e8661fff0cc258ef03394a7
9177siva <sivannarayana49@gmail.com>
2022-04-13 15:47:08 +0530

    Added somecode to Addition.java

--- Addition.java
+++ Addition.java
@@ -5,8 +5,12 @@ public static void main(string args[])
 	int num1,num2,res;
 	num1=2005;
 	num2=4508;
-	res=num1+num2;
+	res=num1+num2/num2-num1;
 	system.out.println("The sum is "+res);
+	if(res>0)
+		system.out.println("this is a positive number");
+	else
+		system.out.println("this is a negative number");
 	//this is comment
 }
 }
